264ec053acf36e3822e3b5f36f2b64bc2b888d0d
Nicolas <nperedoesquivelgmail.com>
2021-08-18 23:13:53 -0400

    Adding logic to forms

--- yumminess/forms.py
+++ yumminess/forms.py
@@ -1,34 +1,80 @@
 from django import forms
-
 from .models import Employee, MenuPlate, MenuOption, Menu, Order
+from datetime import date
 
 
 class EmployeeForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=50)
+    phone_number = forms.CharField(max_length=20)
+    country = forms.CharField(max_length=30)
+    username = forms.CharField(max_length=30)
+    password = forms.PasswordInput()
+
     class Meta:
         model = Employee
-        exclude = ['enabled', 'created_at']
+        exclude = ['enabled', 'created_at', 'user']
+
+    field_order = ['first_name', 'last_name', 'email', 'phone_number', 'country', 'username', 'password']
 
 
 class MenuPlateForm(forms.ModelForm):
+    menu_plate_name = forms.CharField(max_length=30)
+
     class Meta:
         model = MenuPlate
         exclude = ['created_at']
 
 
 class MenuOptionForm(forms.ModelForm):
+    # e_plate = entrance_plate (variable name changed for pep8 paragraph length)
+    # b_plate = bottom_plate (variable name changed for pep8 paragraph length)
+    # d_plate = bottom_plate (variable name changed for pep8 paragraph length)
+    e_plate = 1
+    b_plate = 2
+    d_plate = 3
+
+    menu_option_name = forms.CharField(max_length=30)
+    menu_option_entrance_plate = forms.ModelChoiceField(queryset=MenuPlate.objects.filter(menu_plate_type=e_plate))
+    menu_option_bottom_plate = forms.ModelChoiceField(queryset=MenuPlate.objects.filter(menu_plate_type=b_plate))
+    menu_option_dessert_plate = forms.ModelChoiceField(queryset=MenuPlate.objects.filter(menu_plate_type=d_plate))
 
     class Meta:
         model = MenuOption
         exclude = ['created_at']
 
+    def clean(self, *args, **kwargs):
+        menu_option_entrance_plate = self.cleaned_data.get("menu_option_entrance_plate")
+        menu_option_bottom_plate = self.cleaned_data.get("menu_option_bottom_plate")
+        menu_option_dessert_plate = self.cleaned_data.get("menu_option_dessert_plate")
+
+        print(menu_option_entrance_plate)
+        print(menu_option_bottom_plate)
+        print(menu_option_dessert_plate)
+
 
 class MenuForm(forms.ModelForm):
+    menu_name = forms.CharField(max_length=30)
+    menu_options = forms.ModelMultipleChoiceField(queryset=MenuOption.objects.all())
+
     class Meta:
         model = Menu
         exclude = ['uuid', 'created_at']
 
 
 class OrderForm(forms.ModelForm):
+    menu_options = forms.ModelChoiceField(queryset=MenuOption.objects.none())
+    order_customization = forms.CharField(max_length=400)
+
     class Meta:
         model = Order
         exclude = ['created_at']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        # Only one menu per day (menu has multiple options)
+        menu = Menu.objects.get(created_at=date.today())
+        if menu:
+            options = MenuOption.objects.filter(menu__id=menu.id)
+            self.fields['menu_options'].queryset = options
--- yumminess/models.py
+++ yumminess/models.py
@@ -5,12 +5,14 @@ import uuid
 
 
 class Employee(models.Model):
+    user = models.OneToOneField(User, null=False, on_delete=models.CASCADE, default=None)
     country = models.CharField(max_length=30)
     email = models.EmailField(max_length=50)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone_number = models.CharField(max_length=20)
     username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30, null=False, default=None)
     enabled = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -32,7 +34,7 @@ class MenuPlate(models.Model):
     MENU_PLATE_TYPES = (
         (1, 'Entrance plate'),
         (2, 'Bottom plate'),
-        (3, 'Dessert')
+        (3, 'Dessert plate')
     )
 
     menu_plate_name = models.CharField(max_length=30)
@@ -47,11 +49,11 @@ class MenuOption(models.Model):
     menu_option_name = models.CharField(max_length=40)
     created_at = models.DateTimeField(auto_now_add=True)
     menu_option_entrance_plate = models.ForeignKey(MenuPlate, related_name='menu_option_entrance_plate',
-                                                   limit_choices_to={'menu_plate_type: 1'}, on_delete=models.CASCADE)
+                                                   limit_choices_to={'menu_plate_type': 1}, on_delete=models.CASCADE)
     menu_option_bottom_plate = models.ForeignKey(MenuPlate, related_name='menu_option_bottom_plate',
-                                                 limit_choices_to={'menu_plate_type: 2'}, on_delete=models.CASCADE)
+                                                 limit_choices_to={'menu_plate_type': 2}, on_delete=models.CASCADE)
     menu_option_dessert_plate = models.ForeignKey(MenuPlate, related_name='menu_option_dessert_plate',
-                                                  limit_choices_to={'menu_plate_type: 3'}, on_delete=models.CASCADE)
+                                                  limit_choices_to={'menu_plate_type': 3}, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.menu_option_name)
@@ -60,7 +62,7 @@ class MenuOption(models.Model):
 class Menu(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4())
     menu_name = models.CharField(max_length=30)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
     menu_options = models.ManyToManyField(MenuOption)
 
     def __str__(self):
@@ -69,6 +71,6 @@ class Menu(models.Model):
 
 class Order(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    option = models.ForeignKey(MenuOption, on_delete=models.CASCADE)
+    menu_options = models.ForeignKey(MenuOption, on_delete=models.CASCADE)
     order_customization = models.CharField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
